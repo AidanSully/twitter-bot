b1519142b596d722cfa4399f1777dcc32ad5e24c
aidan s <aidansully@github.com>
2020-10-02 12:49:11 +0200

    connected to twitter

---
+++ node3/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "nvq0x3es7jTw4lQ5ShfJjmWZI",
+  consumer_secret: "E1LRrfvSnWbRNI62JqHMaxMBTdzheLlo2OK4LHm61M42VykBJw",
+  access_token: "1311955382135291905-dXyVXQmuHTjlDmhFGtWmjGHIrOJjSo",
+  access_token_secret: "xb7OElTlkKCTFLFAPj7InYaPj6z89BTPBfn841SW2OnXU",
+};
